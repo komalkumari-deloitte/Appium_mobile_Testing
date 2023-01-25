53bd155665edc4277f4286309f900ba363d90c4f
komalkumari-deloitte <komalkumari@deloitte.com>
2023-01-25 21:07:24 +0530

    Hybrid app automation

--- src/test/java/RealTimeEcommerceApp/EcommerceAppTC_2.java
+++ src/test/java/RealTimeEcommerceApp/EcommerceAppTC_2.java
@@ -1,7 +1,10 @@
 package RealTimeEcommerceApp;
 
 import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -10,6 +13,7 @@ import org.testng.annotations.Test;
 
 import java.time.Duration;
 import java.util.List;
+import java.util.Set;
 
 public class EcommerceAppTC_2 extends BaseTest1{
     @Test(priority = 1)
@@ -26,6 +30,7 @@ public class EcommerceAppTC_2 extends BaseTest1{
     }
     @Test(priority = 2)
     public void Shopping() throws InterruptedException {
+        //shopping for two products
         driver.findElements(By.xpath("//android.widget.TextView[@text='ADD TO CART']")).get(0).click();
         driver.findElements(By.xpath("//android.widget.TextView[@text='ADD TO CART']")).get(0).click();
         //click on cart icon
@@ -36,7 +41,7 @@ public class EcommerceAppTC_2 extends BaseTest1{
         wait.until(ExpectedConditions.attributeContains(driver.findElement(By.id("com.androidsample.generalstore:id/toolbar_title")), "text", "Cart"));
     }
     @Test(priority = 3)
-    public void ValidatingTotalAmount(){
+    public void ValidatingTotalAmount() throws InterruptedException {
         List < WebElement> productPrice=driver.findElements(By.id("com.androidsample.generalstore:id/productPrice"));
         int count=productPrice.size();
         //sum of product prices
@@ -60,7 +65,26 @@ public class EcommerceAppTC_2 extends BaseTest1{
 
         //click on check box
         driver.findElement(AppiumBy.className("android.widget.CheckBox")).click();
+        //click on proceed to purchase
         driver.findElement(By.id("com.androidsample.generalstore:id/btnProceed")).click();
+        Thread.sleep(6000);
+    }
+    @Test(priority = 4)
+    public void Hybrid(){
+        //give us all context from native app to Webview
+        Set<String> context=driver.getContextHandles();
+        //fetching context name through appium
+        for (String contextName:context) //enhanced for loop
+        {
+            System.out.println(contextName);
+        }
+        //name of context- WEBVIEW_com.androidsample.generalstore
+        driver.context("WEBVIEW_com.androidsample.generalstore");
+        driver.findElement(By.name("q")).sendKeys("Shopping apps");
+        driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+        driver.pressKey(new KeyEvent(AndroidKey.BACK));
+        driver.context("NATIVE_APP");
+
 
 
 
